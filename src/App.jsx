98b77d1cdd51c556988eee0bc2e357bb450ea703
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-09-15 06:20:49 +0530

    Tailwind Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home.jsx";
-import Error from "./ui/Error.jsx";
-import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
-import Cart from "./features/cart/Cart.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder.jsx";
-import Order, { loader as orderLoader } from "./features/order/Order.jsx";
-import AppLayout from "./ui/AppLayout.jsx";
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
+
     children: [
       {
         path: "/",
@@ -22,11 +24,9 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
+      { path: "/cart", element: <Cart /> },
       {
         path: "/order/new",
         element: <CreateOrder />,
@@ -36,11 +36,14 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
 ]);
 
-export default function App() {
+function App() {
   return <RouterProvider router={router} />;
 }
+
+export default App;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Bhavya</div>;
+}
+
+export default Username;
